6008b47688ec5a111e937a4e80419177a9eacaef
leofalves <leo.fabiano@gmail.com>
2016-02-28 20:19:09 -0300

    quit i

--- ContactList/Server.js
+++ ContactList/Server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+
 var mongojs = require('mongojs');
 var db = mongojs('contactList',['contactList']);
+
+var bodyParser = require('body-parser')
 
 //app.get('/', function(req, res){
 //	res.send('Hello World from Server')
 //});
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/contactList', function(req, res){
 	console.log('I received a GET request');
@@ -41,6 +45,13 @@ app.get('/contactList', function(req, res){
     */
 
 });
+
+app.post('/contactList', function(req, res){
+	console.log(req.body);
+	db.contactList.insert(req.body, function(err, doc){
+		res.json(doc);
+	})
+})
 
 app.listen(3000);
 console.log('Server is running on port 3000');
--- ContactList/public/controllers/controller.js
+++ ContactList/public/controllers/controller.js
@@ -1,11 +1,24 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    
     console.log("Hello World from controller");
 
-    $http.get('/contactList').success(function(response){
-    	console.log('I got the data I requested');
-    	$scope.contactList = response;
-    });
+    var refresh = function(){
+	    $http.get('/contactList').success(function(response){
+	    	console.log('I got the data I requested');
+	    	$scope.contactList = response;
+	    	$scope.contact = '';
+	    });
+	};
 
+	refresh();
+
+    $scope.addContact = function () {
+    	console.log($scope.contact);
+    	$http.post('/contactList', $scope.contact).success(function(response){
+    		console.log(response);
+    		refresh();
+    	});
+    }
 }]);
